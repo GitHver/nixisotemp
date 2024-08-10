d7c8b0e8266ddf4fae1225e4c94f888cf659a23e
er ég? <165333014+GitHver@users.noreply.github.com>
2024-08-10 19:22:11 +0000

    Delete users directory

--- users/admin.nix
+++
@@ -1,8 +0,0 @@
-
-let
-  admin = {
-    un = "admin";
-    dn = "Administrator";
-    pw = "$6$8ShwXPk0YFHHFMX4$T5m/RNViJGA8LrFo0MViAZ6tmp9EN3e3tK8zRc5iuGqiQQ43YnHgqXWBYEQxuRvgih7L6BfwqGUJGRD4YL3gI/";
-  };
-in admin
