9c978971db94df0d2bfc32f4a61ff7bbd5ab3bff
er ég? <165333014+GitHver@users.noreply.github.com>
2024-08-10 23:58:44 +0000

    Update users.nix

--- hardware/template/users.nix
+++ hardware/template/users.nix
@@ -1,11 +1,11 @@
-{ lib, makeUsers, recursiveMerge, ... }:
+{ lib, makeUsers, recursiveMerge, umport, ... }:
 
 {
  #====<< User management >>====================================================>
-  users.mutableUsers = true;         # Makes the home directory writeable.
+  users.mutableUsers = true;    # Allows imparative user management.
   users.users = let
     t1 = makeUsers {
-      userpath  = ./users;
+      userpath  = umport { path = ./users; };
       userrules = [ "wheel" "networkmanager" ]; };
   in recursiveMerge [ t1 ];
 }
