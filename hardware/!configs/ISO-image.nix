2bba74cd82fbbee6f403784ddaea1f997fed439d
GitHver <hver.vef@gmail.com>
2024-08-28 17:40:29 +0000

    ups1

--- hardware/!configs/ISO-image.nix
+++ hardware/!configs/ISO-image.nix
@@ -52,7 +52,7 @@ in {
     '';
     generate-config = ''
       sudo nixos-generate-config --no-filesystems --root /mnt
-      sudo mv -r ~/${directory}/. /mnt/etc/nixos
+      sudo cp -r ~/${directory}/. /mnt/etc/nixos
       cd /mnt/etc/nixos
       sudo mv ./hardware-configuration.nix ./hardware/template
       sudo rm ./configuration.nix
