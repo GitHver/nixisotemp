08fb3ad23225e4c23d60fe9fa63644cf0b37d8ab
GitHver? <hver.vef@gmail.com>
2024-09-01 17:39:31 +0000

    bcachefs support & latest linux

--- hardware/default/hardware.nix
+++ hardware/default/hardware.nix
@@ -33,6 +33,8 @@ in {
     splashImage = null;
   };
 
+  boot.supportedFilesystems = [ "bcachefs" ];
+  boot.kernelPackages = pkgs.linuxPackages_latest
   # boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_10;
 
   #====<< Luks incryption >>===================================================>
